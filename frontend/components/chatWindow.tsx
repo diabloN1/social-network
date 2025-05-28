81e3f120e63197a484f50589439d07f5a9524870
f <medlfarssi10@gmail.com>
2025-05-28 14:33:51 +0100

    removing consoles and alert , adding popups 3

--- frontend/components/chatWindow.tsx
+++ frontend/components/chatWindow.tsx
@@ -7,6 +7,7 @@ import getMessages from "@/api/messages/getMesages";
 import { addMessage } from "@/helpers/addMessage";
 import { onMessageType, socket } from "@/helpers/webSocket";
 import getToken from "@/api/auth/getToken";
+import Popup from "@/app/app/popup";
 
 interface User {
   id: number;
@@ -42,6 +43,8 @@ export default function ChatWindow({ chat }: ChatWindowProps) {
   const [messages, setMessages] = useState<Message[] | null>(null);
   const messagesEndRef = useRef<HTMLDivElement>(null);
   const messageInputRef = useRef<HTMLTextAreaElement>(null);
+  const [popup, setPopup] = useState<{ message: string; status: "success" | "failure" } | null>(null);
+  
 
   // Scroll to bottom when messages change
   useEffect(() => {
@@ -51,14 +54,13 @@ export default function ChatWindow({ chat }: ChatWindowProps) {
       try {
         const data = await getMessages(id, chat?.isGroup || false);
         if (data.error) {
-          alert(data.error);
+          setPopup({ message: data.error, status: "failure" });
           return;
         }
-        console.log("--------", data);
 
         setMessages(data?.messages);
       } catch (error) {
-        alert(error);
+        setPopup({ message: `${error}`, status: "failure" });
       }
     };
     initMessages();
@@ -91,7 +93,7 @@ export default function ChatWindow({ chat }: ChatWindowProps) {
             })
           );
         } catch (error) {
-          alert(error);
+          setPopup({ message: `${error}`, status: "failure" });
         }
       }
     });
@@ -120,7 +122,7 @@ export default function ChatWindow({ chat }: ChatWindowProps) {
       setMessageInput("");
       setShowEmojis(false);
     } catch (error) {
-      alert(error);
+      setPopup({ message: `${error}`, status: "failure" });
     }
   };
 
@@ -297,6 +299,13 @@ export default function ChatWindow({ chat }: ChatWindowProps) {
           &#10148;
         </button>
       </form>
+      {popup && (
+        <Popup
+          message={popup.message}
+          status={popup.status}
+          onClose={() => setPopup(null)}
+        />
+      )}
     </div>
   );
 }
