3a8be6ddfc349b5c89cd60d0834c59e19e5f2ac8
Fahd Aguenouz <faguenouz@gmail.com>
2025-06-04 11:11:16 +0100

    finding an error in globalapihandler

--- frontend/helpers/GlobalAPIHelper.ts
+++ frontend/helpers/GlobalAPIHelper.ts
@@ -1,5 +1,6 @@
 "use client";
 
+
 import { useError } from "@/context/ErrorContext";
 import { useRouter } from "next/navigation";
 import { useCallback } from "react";
@@ -69,7 +70,8 @@ export const useGlobalAPIHelper = () => {
         return data.data ?? data;
       } catch (err: any) {
         console.error("API call failed:", err);
-        // return handleAPIError(err.message || "Unexpected error", 500);
+        alert(err.data.cause)
+       // return handleAPIError(err.message || "Unexpected error", 500);
       }
     },
     []
