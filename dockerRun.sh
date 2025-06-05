1a198d7a01def2315a96b3f6fd5247ae1530a5cd
myacoubi <amineamoune904@gmail.com>
2025-06-05 18:40:08 +0100

    working on docker

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,11 +1,10 @@
 docker rm -f $(docker ps -aq) && docker rmi -f $(docker images -aq)
 
-# cd frontend
-# npm run build
-# docker build -t frontend .
-# docker run -d -p 3000:3000 --name frontend frontend
+cd frontend
+npm run build
+docker build -t frontend .
+docker run -d -p 3000:3000 --name frontend frontend
 
-cd backend
-go build -o backend
-docker build -t backend .
-docker run -d -p 8080:8080 --name backend backend
+# cd ../backend
+# docker build -t backend .
+# docker run -d -p 8080:8080 --name backend backend
